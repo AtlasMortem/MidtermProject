8d7878f1ba22da508fa38045dc67801b11bed130
AtlasMortem <canuper1033@gmail.com>
2025-03-15 21:21:31 -0700

    Minor patch to include music player on Home page.

--- app.js
+++ app.js
@@ -67,3 +67,48 @@ function sortTable(tableId, n) {
         }
     }
 }
+
+// // Set up the audio player with the buttons to control it
+// const audio = document.getElementById("audio");
+// const playButton = document.getElementById("play");
+// const pauseButton = document.getElementById("pause");
+// const stopButton = document.getElementById("stop");
+
+// // Create an array of songs so that I can add more later
+// const songs = ["media/Enshrined-in-Winters-Ice.mp3"];
+// let currentSongIndex = 0;
+
+// currentSongIndex = (currentSongIndex + 1) % songs.length;
+// audio.src = songs[currentSongIndex];
+// audio.load();
+
+// // Create functions to control the audio player
+// function playSong() {
+//     audio.play();
+// }
+// function pauseSong() {
+//     audio.pause();
+// }
+// function stopSong() {
+//     audio.pause();
+//     // If the song is stopped, reset the track's progress to 0
+//     audio.currentTime = 0;
+// }
+// /*
+// function nextSong() {
+//     currentSongIndex = (currentSongIndex + 1) % songs.length;
+//     audio.src = songs[currentSongIndex];
+//     playSong();
+// }
+// function previousSong() {
+//     currentSongIndex = (currentSongIndex - 1) % songs.length;
+//     audio.src = songs[currentSongIndex];
+//     playSong();
+// } */
+
+// // Add event listeners to the buttons that execute the functions
+// playButton.addEventListener("click", playSong);
+// pauseButton.addEventListener("click", pauseSong);
+// stopButton.addEventListener("click", stopSong);
+// //nextButton.addEventListener("click", nextSong);
+// //previousButton.addEventListener("click", previousSong);
