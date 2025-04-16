c30cee762df5afe5c6baba23eb87ac0a3d4e11c0
AtlasMortem <canuper1033@gmail.com>
2025-04-15 19:35:06 -0700

    Further Attempts at JS Music Player (part 2)

--- app.js
+++ app.js
@@ -68,52 +68,58 @@ function sortTable(tableId, n) {
     }
 }
 
-/* I misspoke earlier. I had planned an extra page for the music player at one point, but
-   I altered my goal to keep the music player within the home page (but make it look more like an embeeded player).
-   The idea was to have a square or rectangular player with its own solid background, and it would read the track name
-   based upon the current index of the playlist. I waffled back and forth between YouTube tutorials and error-checking with ChatGPT, for errors I couldn't detect just by looking at the code. */
+/*
+I misspoke earlier. I had planned an extra page for the music player at one point, but
+I altered my goal to keep the music player within the home page (but make it look more like an embeeded player).
+The idea was to have a square or rectangular player with its own solid background, and it would read the track name
+based upon the current index of the playlist. I waffled back and forth between YouTube tutorials and error-checking with ChatGPT, for errors I couldn't detect just by looking at the code. */
 
-// // Set up the audio player with the buttons to control it
-// const audio = document.getElementById("audio");
-// const playButton = document.getElementById("play");
-// const pauseButton = document.getElementById("pause");
-// const stopButton = document.getElementById("stop");
+/* Here were my sources for the music player:
+https://dev.to/ctrlcoding/how-to-create-a-simple-audio-player-in-html-javascript-and-css-4l2e
+...I didn't keep track of my other sources, and I can't seem to find them now. I apologize for that. I watched at least three videos on it, though, and even when I followed their instructions, it didn't come out quite right. Some even tried going into a different kind of editor, but it didn't make sense to me, so I moved on to different videos.
+I know more about the code now than I did when I started, so I did another review before sending this, but I still can't get it to work.
+*/
 
-// // Create an array of songs so that I can add more later
-// const songs = ["media/Enshrined-in-Winters-Ice.mp3"];
-// let currentSongIndex = 0;
+// Set up the audio player with the buttons to control it
+const audio = document.getElementById("audio");
+const playButton = document.getElementById("play");
+const pauseButton = document.getElementById("pause");
+const stopButton = document.getElementById("stop");
 
-// currentSongIndex = (currentSongIndex + 1) % songs.length;
-// audio.src = songs[currentSongIndex];
-// audio.load();
+// Create an array of songs so that I can add more later
+const songs = ["media/Enshrined-in-Winters-Ice.mp3"];
+let currentSongIndex = 0;
 
-// // Create functions to control the audio player
-// function playSong() {
-//     audio.play();
-// }
-// function pauseSong() {
-//     audio.pause();
-// }
-// function stopSong() {
-//     audio.pause();
-//     // If the song is stopped, reset the track's progress to 0
-//     audio.currentTime = 0;
-// }
-// /*
-// function nextSong() {
-//     currentSongIndex = (currentSongIndex + 1) % songs.length;
-//     audio.src = songs[currentSongIndex];
-//     playSong();
-// }
-// function previousSong() {
-//     currentSongIndex = (currentSongIndex - 1) % songs.length;
-//     audio.src = songs[currentSongIndex];
-//     playSong();
-// } */
+currentSongIndex = (currentSongIndex + 1) % songs.length;
+audio.src = songs[currentSongIndex];
+audio.load();
 
-// // Add event listeners to the buttons that execute the functions
-// playButton.addEventListener("click", playSong);
-// pauseButton.addEventListener("click", pauseSong);
-// stopButton.addEventListener("click", stopSong);
-// // nextButton.addEventListener("click", nextSong);
-// // previousButton.addEventListener("click", previousSong);
+// Create functions to control the audio player
+function playSong() {
+  audio.play();
+}
+function pauseSong() {
+  audio.pause();
+}
+function stopSong() {
+  audio.pause();
+  // If the song is stopped, reset the track's progress to 0
+  audio.currentTime = 0;
+}
+
+function nextSong() {
+  currentSongIndex = (currentSongIndex + 1) % songs.length;
+  audio.src = songs[currentSongIndex];
+  playSong();
+}
+function previousSong() {
+  currentSongIndex = (currentSongIndex - 1) % songs.length;
+  audio.src = songs[currentSongIndex];
+  playSong();
+} 
+// Add event listeners to the buttons that execute the functions
+playButton.addEventListener("click", playSong);
+pauseButton.addEventListener("click", pauseSong);
+stopButton.addEventListener("click", stopSong);
+nextButton.addEventListener("click", nextSong);
+previousButton.addEventListener("click", previousSong);
